4ee4ba6ce218553dae2cdd6a4b48a67a5649eb4c
Girish Mahale <girishmahale786@gmail.com>
2024-11-12 06:06:43 +0530

    added reset password

--- accounts/views.py
+++ accounts/views.py
@@ -16,3 +16,29 @@ def verify_email(request):
                 {"message": "Email verified successfully"},
             )
     return render(request, "account/verify_email.html")
+
+def reset_password(request):
+    uid = request.GET.get("uid", None)
+    token = request.GET.get("token", None)
+
+    if request.method == "POST":
+        uid = request.POST.get("uid", None)
+        token = request.POST.get("token", None)
+        passwd1 = request.POST.get("passwd1", None)
+        passwd2 = request.POST.get("passwd2", None)
+        if passwd1 and passwd2 and passwd1 != passwd2:
+            return render(
+                request,
+                "account/reset_password.html",
+                {"message": "Passwords do not match"},
+            )
+        url = f"https://{request.get_host()}/api/auth/password/reset/confirm/"
+        res = requests.post(url, data={"uid": uid, "token": token, "new_password1": passwd1, "new_password2": passwd2})
+        if res.status_code == 200:
+            return render(
+                request,
+                "account/reset_password.html",
+                {"message": "Password reset successfully"},
+            )
+        return render(request, "account/reset_password.html", {"message": "Error resetting password."})
+    return render(request, "account/reset_password.html", {"uid": uid, "token": token})
--- proofvision/urls.py
+++ proofvision/urls.py
@@ -20,10 +20,11 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from .routers import router
-from accounts.views import verify_email
+from accounts.views import verify_email, reset_password
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("account/confirm-email/", verify_email, name="verify_email"),
+    path("account/reset-password/", reset_password, name="reset_password"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
